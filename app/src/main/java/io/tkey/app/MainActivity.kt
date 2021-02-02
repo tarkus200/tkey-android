0bdc63f78bae4bac2d78118023468642d2ac5eda
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-02-02 11:40:20 +0700

    Initial setup

---
+++ app/src/main/java/io/tkey/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package io.tkey.app
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.text)
+        textView.text = "Hello, World!"
+    }
+}
