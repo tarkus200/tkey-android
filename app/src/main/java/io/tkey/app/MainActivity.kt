63a4560cadf43c0f72058ee020f47580406a4cc0
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-02-03 12:19:10 +0700

    Add basic UI for example app

--- app/src/main/java/io/tkey/app/MainActivity.kt
+++ app/src/main/java/io/tkey/app/MainActivity.kt
@@ -1,16 +1,11 @@
 package io.tkey.app
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import io.tkey.types.TKeyException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val textView = findViewById<TextView>(R.id.text)
-        textView.text = TKeyException(1).toString()
     }
 }
